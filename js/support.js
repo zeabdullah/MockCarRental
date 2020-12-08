94b6c1084fbc01c95f15d47c6864a1ac6c5d7be9
Abdullah Zeidan <abdullahzeidan@hotmail.com>
2020-12-08 19:42:39 +0200

    Added bootstrap-alert to support page

--- js/support.js
+++ js/support.js
@@ -1,3 +1,19 @@
-let alertMSG = () => {
-  alert("Thank you for your Support!");
-}
+const $alertContainer = $('.alert-container');
+const $form = $('form');
+
+$alertContainer.hide();
+
+$form.submit(e => {
+  e.preventDefault();
+
+  $alertContainer
+  .before(`
+    <div class="spinner-border text-primary mb-3" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>`);
+
+  setTimeout(() => {
+    $('.spinner-border').remove();
+    $alertContainer.show();
+  }, 2500);
+})
